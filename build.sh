f08d8b9814e320b81633e4232506f4fa2c7fdc58
Dmitrij Fedorenko <job@fedorenko-d.ru>
2025-06-24 12:25:29 +0400

    Move bindings to artifacts after build

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 napi build --platform --release $@
 echo -e "import { ConnectionOptions } from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
+mkdir -p artifacts
+mv surrealdb.node.*.node artifacts/
