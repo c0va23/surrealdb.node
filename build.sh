5e50daa7e1238bf566b2abe16232fd83998102c1
Dmitrij Fedorenko <job@fedorenko-d.ru>
2025-06-29 09:13:52 +0400

    Move artifacts in build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ napi build --platform --release $@
 echo -e "import { ConnectionOptions } from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
 mkdir -p artifacts
 mv surrealdb.node.*.node artifacts/
+node move_artifacts.js
