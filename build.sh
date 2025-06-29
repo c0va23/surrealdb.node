ed4e1bb53928062e26832df9aaa51985f74cee25
Dmitrij Fedorenko <job@fedorenko-d.ru>
2025-06-29 09:24:24 +0400

    Fix updating index.d.ts

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 napi build --platform --release $@
-echo -e "import { ConnectionOptions } from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
+echo -e "import {\n\tConnectionOptions,\n\tsurrealdbNodeEngines as embeddedSurrealdbNodeEngines,\n} from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
+echo -e '\nexport declare const surrealdbNodeEngines: typeof embeddedSurrealdbNodeEngines' >> index.d.ts
 mkdir -p artifacts
 mv surrealdb.node.*.node artifacts/
 node move_artifacts.js
