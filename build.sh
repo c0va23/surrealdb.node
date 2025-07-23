cc951d04e80ae20958d665a92706039b2ce4b7dc
Dmitrij Fedorenko <job@fedorenko-d.ru>
2025-07-23 11:19:15 +0400

    Don't overwrite index.d.ts

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 napi build --platform --release $@
 napi build --platform --target x86_64-unknown-linux-musl --release $@
-echo -e "import {\n\tConnectionOptions,\n\tsurrealdbNodeEngines as embeddedSurrealdbNodeEngines,\n} from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
-echo -e '\nexport declare const surrealdbNodeEngines: typeof embeddedSurrealdbNodeEngines' >> index.d.ts
+if ! grep embeddedSurrealdbNodeEngines index.d.ts; then
+	echo -e "import {\n\tConnectionOptions,\n\tsurrealdbNodeEngines as embeddedSurrealdbNodeEngines,\n} from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
+	echo -e '\nexport declare const surrealdbNodeEngines: typeof embeddedSurrealdbNodeEngines' >> index.d.ts
+fi
 mkdir -p artifacts
 mv surrealdb.node.*.node artifacts/
 node move_artifacts.js
