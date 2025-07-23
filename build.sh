6682316f82339b8d8d75730aedc2934eb846a656
Dmitrij Fedorenko <job@fedorenko-d.ru>
2025-07-23 11:16:17 +0400

    Add build for musl

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 napi build --platform --release $@
+napi build --platform --target x86_64-unknown-linux-musl --release $@
 echo -e "import {\n\tConnectionOptions,\n\tsurrealdbNodeEngines as embeddedSurrealdbNodeEngines,\n} from \"./lib-src/embedded.js\";\n$(cat index.d.ts)" > index.d.ts
 echo -e '\nexport declare const surrealdbNodeEngines: typeof embeddedSurrealdbNodeEngines' >> index.d.ts
 mkdir -p artifacts
